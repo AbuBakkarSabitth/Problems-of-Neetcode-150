945b2c10d07b847a24054ea47f6e2ad710348391
Abu Bakkar Sabith <140878004+AbuBakkarSabitth@users.noreply.github.com>
2026-02-25 10:17:09 +0600

    Implement palindrome checking function
    Added a function to check if a string is a palindrome, ignoring non-alphanumeric characters and case.

--- ValidSudoku.cpp
+++ ValidSudoku.cpp
@@ -1,2 +1,49 @@
-class Solution {
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
 public:
+    bool isPalindrome(string s){
+    int left =0;
+    int right = s.size()-1;
+    while(left<right){
+
+        while(left<right && !isalnum(s[left])){
+            left++;
+        }
+        while(left <right && !isalnum(s[right])){
+            right--;
+        }
+        if(tolower(s[left])!= tolower(s[right])){
+            return false;
+        }
+        left++;
+        right--;
+
+    }
+    return true;
+
+
+    }
+
+
+};
+int main(){
+
+Solution s;
+string in;
+
+getline(cin,in);
+bool result  = s.isPalindrome(in);
+if(result ==0){
+    cout<<"Is not palindrome"<<endl;
+}
+else{
+    cout<<"Is palindrome"<<endl;
+}
+
+
+
+
+}
+
